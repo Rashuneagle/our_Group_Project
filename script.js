5bfc511f0d4153fa584d103d4bcc69a9d34f2c6a
Zariah <zaripull18@gmail.com>
2024-02-12 20:26:37 -0600

    fixed words list

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ close.addEventListener('click', function(){
 
  });
 
-
+ var wordsElement = document.getElementById("words");
 
 function getWords() {
 
@@ -128,16 +128,19 @@ const wordsAPICall = {
   }
 };
 
+
+
 $.ajax(wordsAPICall).done(function (response) {
+   $("#words").empty();
+  //  document.getElementById("words").innerHTML = "";
   displayWords(response.rhymes.all);
 });
 
 }
 
 function displayWords(data) {
-
-var wordsElement = document.getElementById("words");
-
+//  wordsElement.children = [];
+//  wordsElement.appendChild([]);
 for (let i = 0; i < data.length; i++) {
 
 var listElement = document.createElement("li");
